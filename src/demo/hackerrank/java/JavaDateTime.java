01c295746380a76eac6f74009d1bb382224a3078
Md. Tanvir Hossain <mdttanvirhossain@gmail.com>
2020-01-14 00:10:05 +0600

    jdjasd

--- src/demo/hackerrank/java/JavaDateTime.java
+++ src/demo/hackerrank/java/JavaDateTime.java
@@ -2,7 +2,6 @@ package demo.hackerrank.java;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -16,19 +15,14 @@ public class JavaDateTime {
 		String yyyy = scan.next();
 
 		Integer intdd = Integer.parseInt(dd);
-		intdd = intdd+1;
+		intdd = intdd + 1;
 		String strdd = Integer.toString(intdd);
-		
+
 		String fullDate = strdd + "/" + mm + "/" + yyyy;
-		
 
 		Date newDate = new SimpleDateFormat("dd/MM/yyyy").parse(fullDate);
-		
-		System.out.println(newDate);
-		
 		SimpleDateFormat simpleDateformat = new SimpleDateFormat("EEEE");
 		System.out.println(simpleDateformat.format(newDate).toUpperCase());
-		
 
 	}
 }
